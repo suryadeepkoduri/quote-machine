3fd2e7a1b787ff89794552f92e1fe088d2a39646
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-02-20 22:33:55 +0530

    feat: add quotes and load a quote randomly
    - added some quotes in "quotes.json"
    - implemented logic to load quote randomly
    - added new font named 'NTR' for proper telugu display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import quotes from "./quotes.json"
+import { useEffect } from "react";
+import quotes from "./quotes.json";
+import { useState } from "react";
 
 function App() {
-  document.title = "Quote Machine - Surya Deep Koduri"
+  document.title = "Quote Machine - Surya Deep Koduri";
+  const [randomQuote, setRandomQuote] = useState("");
+
+  useEffect(getRandomQuote, []);
+
+  function getRandomQuote() {
+    const quoteNumber = Math.floor(Math.random() * quotes.length);
+    setRandomQuote(quotes[quoteNumber]);
+  }
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen bg-emerald-100">
-        <div className="border p-3 rounded bg-white shadow-md m-3 max-w-xl">
-          <h1 className="text-5xl font-semibold font-serif leading-normal">{quotes[0].quote}</h1>
-          <p className="font-medium text-2xl text-slate-800 mt-4 text-right">- {quotes[0].author}</p>
+      <div className="flex justify-center items-center min-h-screen bg-linear-to-r from-red-400 to-blue-400">
+        <div className="border p-3 rounded bg-white shadow-md m-3 max-w-4xl">
+          <h1 className="text-5xl leading-normal ntr-regular">{randomQuote.quote}</h1>
+          <p className="font-medium text-2xl text-slate-800 mt-4 text-right ntr-regular">
+            - {randomQuote.movie}
+          </p>
         </div>
       </div>
     </>
