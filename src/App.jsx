0d0d0cedfba70a0171e0c43f3c31652998865a36
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-03-10 19:30:01 +0530

    feat: add generate effect while quote display
    - Improved UI
    - Added "New Quote" button to get new quote without refreshing the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
-import quotes from "./quotes.json";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import quotes from "./quotes.json"
+import { TextGenerateEffect } from "./components/ui/text-generate-effect";
 
 function App() {
   document.title = "Quote Machine - Surya Deep Koduri";
-  const [randomQuote, setRandomQuote] = useState("");
+  const [randomQuote, setRandomQuote] = useState({quote:""});
 
   useEffect(getRandomQuote, []);
 
@@ -12,17 +12,22 @@ function App() {
     const quoteNumber = Math.floor(Math.random() * quotes.length);
     setRandomQuote(quotes[quoteNumber]);
   }
+
   return (
-    <>
-      <div className="flex justify-center items-center min-h-screen bg-linear-to-r from-red-400 to-blue-400">
-        <div className="border p-3 rounded bg-white shadow-md m-3 max-w-4xl">
-          <h1 className="text-5xl leading-normal ntr-regular">{randomQuote.quote}</h1>
-          <p className="font-medium text-2xl text-slate-800 mt-4 text-right ntr-regular">
-            - {randomQuote.movie}
-          </p>
-        </div>
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-purple-500 via-pink-500 to-red-400">
+      <div className="border p-6 rounded-lg bg-black shadow-2xl m-3 max-w-4xl text-center">
+        <TextGenerateEffect key={randomQuote.quote} duration={2} filter={false} words={randomQuote.quote}  className="ntr-regular"/>
+        <p className="font-medium text-2xl text-white mt-4 text-right ntr-regular">
+          - {randomQuote.movie}
+        </p>
+        <button
+          onClick={getRandomQuote}
+          className="mt-6 px-6 py-2 bg-gray-500 text-white rounded shadow-lg hover:bg-gray-600 transition duration-300 float-start"
+        >
+          New Quote
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
